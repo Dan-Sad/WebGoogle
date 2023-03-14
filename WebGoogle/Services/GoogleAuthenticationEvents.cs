6b69eeb4246114d1d37822c5e5b8779c81d137cf
Даниил Садыков <sad1dani@mail.ru>
2023-03-14 11:46:30 +0300

    Google auth to service

--- WebGoogle/Services/GoogleAuthenticationEvents.cs
+++ WebGoogle/Services/GoogleAuthenticationEvents.cs
@@ -1,26 +1,55 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OAuth;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
 
 namespace WebGoogle.Services
 {
-    public class GoogleAuthenticationEvents : OAuthEvents
-    {
-        private readonly GoogleAuthenticationService _googleAuthService;
+	public class GoogleAuthenticationEvents : OAuthEvents
+	{
+		public override async Task CreatingTicket(OAuthCreatingTicketContext context)
+		{
+			var accessToken = context.AccessToken;
+			if (accessToken == null) throw new Exception("AccessToken is null.");
 
-        public GoogleAuthenticationEvents(GoogleAuthenticationService googleAuthService)
-        {
-            _googleAuthService = googleAuthService;
-        }
+			var claimsPrincipal = await GetClaimsPrincipalAsync(accessToken);
+			await context.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+		}
 
-        public override async Task CreatingTicket(OAuthCreatingTicketContext context)
-        {
-            var accessToken = context.AccessToken;
-            if (accessToken == null) throw new Exception("AccessToken is null.");
+		public async Task<ClaimsPrincipal> GetClaimsPrincipalAsync(string accessToken)
+		{
+			var request = new HttpRequestMessage(HttpMethod.Get, Constants.GoogleUserInfoUrl);
+			request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
-            var claimsPrincipal = await _googleAuthService.GetClaimsPrincipalAsync(accessToken);
-            await context.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
-        }
-    }
+			var httpClient = new HttpClient();
+			var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+
+			if (!response.IsSuccessStatusCode)
+			{
+				throw new Exception($"Failed to get user info from Google. Status code: {response.StatusCode}");
+			}
+
+			var userInfo = JObject.Parse(await response.Content.ReadAsStringAsync());
+			string id = userInfo.Value<string>("id");
+			string email = userInfo.Value<string>("email");
+
+			if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(email))
+			{
+				throw new Exception("Failed to retrieve user ID or email from Google.");
+			}
+
+			var claims = new List<Claim>
+		{
+			new Claim(ClaimTypes.NameIdentifier, id),
+			new Claim(ClaimTypes.Email, email),
+		};
+
+			var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+			return new ClaimsPrincipal(claimsIdentity);
+		}
+	}
 
 }
